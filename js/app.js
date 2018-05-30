d8dcf1db3e2eee8cda820e48cfc80ebdf7e3a6d2
slewis96 <seblewis96@gmail.com>
2018-05-30 09:52:40 +0100

    completed comments

--- js/app.js
+++ js/app.js
@@ -4,72 +4,72 @@ var games = 0;
 var compgames = 0;
 // var scoreboard = document.getElementById("scoreboard");
 function beginGame(){
-  //get user input
-    var move = prompt("Enter your choice","(rock, paper, or scissors)");
-    move.toLowerCase();
-  //don't continue until correct input
-    if(move!=="rock"&&move!=="paper"&&move!=="scissors"&&move!=="raygun"){
-      alert("invalid input");
+//get user input
+  var move = prompt("Enter your choice","(rock, paper, or scissors)");
+  move.toLowerCase();
+//don't continue until correct input
+  if(move!=="rock"&&move!=="paper"&&move!=="scissors"&&move!=="raygun"){
+    alert("invalid input");
+    beginGame();
+  }
+  else{
+  //get comp move
+    var compmove = getCompMove();
+    alert(move+" "+compmove);
+  //compare moves get winner bool
+    var result = calculateWinner(move, compmove);
+    if(result==undefined){
+      alert("Draw!");
+      alert("You: " + score + "   Computer: " + compscore);
       beginGame();
     }
-    else{
-    //get comp move
-      var compmove = getCompMove();
-      alert(move+" "+compmove);
-    //compare moves get winner bool
-      var result = calculateWinner(move, compmove);
-      if(result==undefined){
-        alert("Draw!");
-        alert("You: " + score + "   Computer: " + compscore);
+    if(result==true){
+      alert("Win!");
+      score += 1;
+      alert("You: " + score + "   Computer: " + compscore);
+    //you win
+      if(score==3){
+        alert("Congratulations you win!");
+        score=0;
+        compscore=0;
+        games++;
+      //useless html stuff
+        document.getElementById("scoreboard").innerHTML =
+          "<br>Scoreboard(bo5):<br>"+
+          "Your game wins: "+games+" <br>"+
+          "Computer game wins: "+compgames;
+        document.getElementById("img").setAttribute("src", "imgs/victory.gif");
+        document.getElementById("button").setAttribute("onclick","endGame()");
+        document.getElementById("button").innerHTML = "Play again?";
+      } else {
         beginGame();
       }
-      if(result==true){
-        alert("Win!");
-        score += 1;
-        alert("You: " + score + "   Computer: " + compscore);
-      //you win
-        if(score==3){
-          alert("Congratulations you win!");
-          score=0;
-          compscore=0;
-          games++;
-        //useless html stuff
-          document.getElementById("scoreboard").innerHTML =
-            "<br>Scoreboard(bo5):<br>"+
-            "Your game wins: "+games+" <br>"+
-            "Computer game wins: "+compgames;
-          document.getElementById("img").setAttribute("src", "imgs/victory.gif");
-          document.getElementById("button").setAttribute("onclick","endGame()");
-          document.getElementById("button").innerHTML = "Play again?";
-        } else {
-          beginGame();
-        }
-      }
-      if(result==false){
-        alert("Loss!");
-        compscore += 1;
-        alert("You: " + score + "   Computer: " + compscore);
-      //comp wins
-        if(compscore==3){
-          alert("Gameover computer wins!");
-          score=0;
-          compscore=0;
-          score=0;
-          compscore=0;
-          compgames++;
-        //useless html stuff
-          document.getElementById("scoreboard").innerHTML =
-            "<br>Scoreboard(bo5):<br>"+
-            "Your game wins: "+games+" <br>"+
-            "Computer game wins: "+compgames;
-          document.getElementById("img").setAttribute("src", "imgs/failure.gif");
-          document.getElementById("button").setAttribute("onclick","endGame()");
-          document.getElementById("button").innerHTML = "Play again?";
-        } else {
-          beginGame();
-        }
+    }
+    if(result==false){
+      alert("Loss!");
+      compscore += 1;
+      alert("You: " + score + "   Computer: " + compscore);
+    //comp wins
+      if(compscore==3){
+        alert("Gameover computer wins!");
+        score=0;
+        compscore=0;
+        score=0;
+        compscore=0;
+        compgames++;
+      //useless html stuff
+        document.getElementById("scoreboard").innerHTML =
+          "<br>Scoreboard(bo5):<br>"+
+          "Your game wins: "+games+" <br>"+
+          "Computer game wins: "+compgames;
+        document.getElementById("img").setAttribute("src", "imgs/failure.gif");
+        document.getElementById("button").setAttribute("onclick","endGame()");
+        document.getElementById("button").innerHTML = "Play again?";
+      } else {
+        beginGame();
       }
     }
+  }
 }
 function getCompMove(){
   var rand = Math.floor((Math.random() * 3));
@@ -83,25 +83,25 @@ function getCompMove(){
   }
 }
 function calculateWinner(mv1, mv2){
-  //raygun
+//raygun
   if(mv1=="raygun"){
     return true;
   }
-  //rock
+//rock
   if(mv1=="rock" && mv2=="scissors"){
     return true;
   }
   if(mv1=="rock" && mv2=="paper"){
     return false;
   }
-  //paper
+//paper
   if(mv1=="paper" && mv2=="rock"){
     return true;
   }
   if(mv1=="paper" && mv2=="scissors"){
     return false;
   }
-  //scissors
+//scissors
   if(mv1=="scissors" && mv2=="paper"){
     return true;
   }
@@ -113,6 +113,7 @@ function calculateWinner(mv1, mv2){
   }
 }
 function endGame(){
+//useless html stuff
   document.getElementById("img").setAttribute("src", "imgs/rps.gif");
   document.getElementById("button").setAttribute("onclick","beginGame()");
   document.getElementById("button").innerHTML = "Start game";
